07782d4fed0537db6ea1565eb5d3fab7c158313e
arturzaczek1 <artur.zaczek@tlen.pl>
2018-11-22 21:56:12 +0100

    initial commit

---
+++ src/main/java/pl/arturzaczek/demo/Category.java
@@ -0,0 +1,16 @@
+package pl.arturzaczek.demo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+
+public class Category {
+    private Integer id;
+    private Integer parentId;
+    private Integer depth;
+    private String name;
+}
