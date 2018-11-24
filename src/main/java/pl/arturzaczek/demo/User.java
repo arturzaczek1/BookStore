a8c686c7c04bb7c1c4c2ca646e3439c3edec9f32
arturzaczek1 <artur.zaczek@tlen.pl>
2018-11-24 18:22:50 +0100

    added user and userAddress class

--- src/main/java/pl/arturzaczek/demo/User.java
+++ src/main/java/pl/arturzaczek/demo/User.java
@@ -1,4 +1,20 @@
 package pl.arturzaczek.demo;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Setter
+@Getter
+public class User implements Serializable {
+    private static final long serialVersionUID = -2911074201859688764L;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+    private String passwordHash;
+    private UserAddress userAddress;
+    private String birthDate;
+    private String pesel;
+    private String phone;
+    private boolean preferEmails;
 }
--- src/main/java/pl/arturzaczek/demo/UserAddress.java
+++ src/main/java/pl/arturzaczek/demo/UserAddress.java
@@ -1,4 +1,15 @@
 package pl.arturzaczek.demo;
 
-public class UserAddress {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Setter
+@Getter
+public class UserAddress implements Serializable {
+    private static final long serialVersionUID = -8441949325468918622L;
+    private String city;
+    private String country;
+    private String zipCode;
+    private String street;
 }
